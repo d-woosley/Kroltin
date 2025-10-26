6e1764a85061bab55d5171bc1568a23110a90a02
d-woosley <duncan.woosley@gmail.com>
2025-10-26 17:09:54 -0500

    Add self to kali-trused group in kali-grant-root.sh

--- kroltin/scripts/kali-grant-root.sh
+++ kroltin/scripts/kali-grant-root.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 apt install kali-grant-root -y
 printf 'kali-grant-root kali-grant-root/enable boolean true\n' | debconf-set-selections
 DEBIAN_FRONTEND=noninteractive dpkg-reconfigure -f noninteractive -p low kali-grant-root
+
+# Add Self to kali-trusted
+usermod -aG kali-trusted "$(whoami)"
